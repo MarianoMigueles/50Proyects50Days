73ff1589636d31b506ced419577135d81de811b3
MarianoMigueles <164581983+MarianoMigueles@users.noreply.github.com>
2024-05-24 22:10:16 -0300

    Delete 03-RotatingNavigationAnimation directory

--- 03-RotatingNavigationAnimation/Js/main.js
+++
@@ -1,7 +0,0 @@
-const btn_open_menu = document.getElementById('btn-burguer')
-const btn_close_menu = document.getElementById('btn-close')
-const container = document.querySelector('.container') 
-
-btn_open_menu.addEventListener('click', ()=> container.classList.add('show-nav'))
-
-btn_close_menu.addEventListener('click', ()=> container.classList.remove('show-nav'))
